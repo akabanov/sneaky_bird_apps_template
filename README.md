95a3fa1a934411bf71aafb84711da7fd64cba38c
akabanov <alexander.kabanoff@gmail.com>
2024-11-17 19:16:29 +1300

    Add integration testing setup
    Updated main widget structure and .gitignore. Added integration and instrumented test configurations, dependencies, and example tests to support screenshot capturing and counter increment functionality. Updated `pubspec.yaml` and `build.gradle` to include necessary dependencies.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # flutter_skeleton_app
 
 A new Flutter project.
+
+## Running mobile integration tests
+
+```shell
+flutter drive "--driver=test_driver/integration_test.dart" "--target=integration_test/main_test.dart"
+```
---
+++ test_driver/integration_test.dart
@@ -0,0 +1,20 @@
+
+import 'dart:io';
+
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() async {
+  await integrationDriver(
+    onScreenshot: (String screenshotName, List<int> screenshotBytes,
+        [Map<String, Object?>? args]) async {
+      final Directory screenshotsDir = Directory('screenshots');
+      if (!screenshotsDir.existsSync()) {
+        screenshotsDir.createSync(recursive: true);
+      }
+      final File image = File('screenshots/$screenshotName.png');
+      image.writeAsBytesSync(screenshotBytes);
+      // Return false if the screenshot is invalid.
+      return true;
+    },
+  );
+}
