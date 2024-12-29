c9cab42caa21955cdddeb69f4953804c621de163
akabanov <alexander.kabanoff@gmail.com>
2024-12-29 16:36:32 +1300

    Add setup script draft

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+TEMPLATE_PROJECT_NAME=${PWD##*/}
+
 read -r -p "Enter the app domain: " APP_DOMAIN
 #APP_DOMAIN=${APP_DOMAIN:-"sneakybird.app"}
-read -r -p "Enter the app name [${PWD##*/}]: " APP_NAME
+read -r -p "Enter the app name [${TEMPLATE_PROJECT_NAME}]: " APP_NAME
 #APP_NAME=${APP_NAME:-"recall_digits"}
 APP_NAME=${APP_NAME:-${PWD##*/}}
 
@@ -11,15 +13,15 @@ JAVA_PKG_PATH="${REVERSE_DOMAIN//./\/}"
 
 echo
 echo "Renaming files and directories to ${APP_NAME}"
-find . -type f -not -path '*/.git/*' -exec sed -i "s/recall_digits/${APP_NAME}/g" {} +
-find . -depth -name "*recall_digits*" -not -path '*/.git/*' -execdir bash -c 'mv "$1" "${1//recall_digits/'"${APP_NAME}"'}"' _ {} \;
+find . -type f -not -path '*/.git/*' -exec sed -i "s/${TEMPLATE_PROJECT_NAME}/${APP_NAME}/g" {} +
+find . -depth -name "*${TEMPLATE_PROJECT_NAME}*" -not -path '*/.git/*' -execdir bash -c 'mv "$1" "${1//${TEMPLATE_PROJECT_NAME}/'"${APP_NAME}"'}"' _ {} \;
 echo "Done"
 
 echo
 echo "Renaming Java packages"
 JAVA_PKG_ROOTS=("android/app/src/androidTest/java" "android/app/src/main/kotlin")
 for path in "${JAVA_PKG_ROOTS[@]}"; do
-# skip on subsequent pass
+# skip on subsequent runs
   if [ -d "${path}" ]; then
     mkdir -p "${path}/${JAVA_PKG_PATH}"
     mv "${path}"/com/example/* "${path}/${JAVA_PKG_PATH}"
@@ -29,6 +31,12 @@ done
 git add -A .
 echo "Done"
 
+if [ -d ".idea" ]; then
+  echo
+  echo "Deleting IDEA project files"
+  rm -rf .idea
+  echo "Done"
+fi
 
 #read -pr "Enter Google billing account ID (https://console.cloud.google.com/billing) [${GCLOUD_BILLING_ACCOUNT_ID}]: " BILLING_ACCOUNT_ID
 #BILLING_ACCOUNT_ID=${BILLING_ACCOUNT_ID:-${GCLOUD_BILLING_ACCOUNT_ID}}
