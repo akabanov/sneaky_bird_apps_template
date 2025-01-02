844dd519da193139b0c580316673fd78146b1f61
akabanov <alexander.kabanoff@gmail.com>
2025-01-02 22:35:00 +1300

    Refactor nuke script.
    Consolidated the logic from `nuke-codemagic-apps.sh` into `nuke.sh` and removed the separate script. Simplified functionality by directly using environment variables and prompting for Codemagic deletions. Added placeholders for future integrations with App Store Connect, GCloud, and GitHub.

--- nuke.sh
+++ nuke.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-source ./nuke-codemagic-apps.sh
+. .env
+
+### Codemagic
+
+read -r -p "Delete Codemagic application '${APP_NAME_SNAKE}'? (y/N) " YN
+if [[ "$YN" =~ ^[yY] ]]; then
+  curl \
+    -H "Content-Type: application/json" \
+    -H "x-auth-token: ${CODEMAGIC_API_TOKEN}" \
+    --request DELETE "https://api.codemagic.io/apps/${CODEMAGIC_APP_ID}"
+  echo
+fi
+
+### App store connect
+
+# TODO
+
+### GCLOUD
+
+# TODO
+
+### Git Hub repo
+
+gh repo delete "$APP_NAME_SNAKE"
