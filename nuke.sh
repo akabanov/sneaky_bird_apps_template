b40467a9fed5027fe1025e6474680f6b2646f672
akabanov <alexander.kabanoff@gmail.com>
2025-01-18 00:32:30 +1300

    Add nuke script support for GCloud, Sentry, and Shorebird
    Expanded the `nuke.sh` script to include support for deleting GCloud projects, Sentry projects, and provided a manual guide for Shorebird deletions. These improvements streamline the cleanup process across multiple services while maintaining interactive confirmations. Codemagic deletion handling was updated for better consistency.

--- nuke.sh
+++ nuke.sh
@@ -2,25 +2,30 @@
 
 . .env
 
-### Codemagic
-
 read -r -p "Delete Codemagic application '${APP_NAME_SNAKE}'? (y/N) " YN
 if [[ "$YN" =~ ^[yY] ]]; then
-  curl \
-    -H "Content-Type: application/json" \
+#    -H "Content-Type: application/json" \
+  curl  "https://api.codemagic.io/apps/${CODEMAGIC_APP_ID}" \
     -H "x-auth-token: $(cat "$CM_API_TOKEN_PATH")" \
-    --request DELETE "https://api.codemagic.io/apps/${CODEMAGIC_APP_ID}"
+    -X DELETE
   echo
 fi
 
-### App store connect
+read -r -p "Delete GCloud project '${APP_ID_SLUG}'? (y/N) " YN
+if [[ "$YN" =~ ^[yY] ]]; then
+  gcloud projects delete "$APP_ID_SLUG" --quiet
+fi
 
-# TODO
+read -r -p "Delete Sentry project '${APP_ID_SLUG}'? (y/N) " YN
+if [[ "$YN" =~ ^[yY] ]]; then
+  curl "https://sentry.io/api/0/projects/${SENTRY_ORG_NAME}/${APP_ID_SLUG}/" \
+   -H "Authorization: Bearer $(cat "$SENTRY_PROJECTS_ADMIN_TOKEN_PATH")" \
+   -X DELETE
+   echo
+fi
 
-### GCLOUD
-
-# TODO
-
-### Git Hub repo
-
-read -r -p "Delete GitHub repository '${APP_NAME_SNAKE}'? (y/N) " YN && [[ "$YN" =~ ^[yY] ]] && gh repo delete --yes "$APP_NAME_SNAKE"
+read -r -p "Delete Shorebird project? (y/N) " YN
+if [[ "$YN" =~ ^[yY] ]]; then
+  echo "You need to do this manually at https://console.shorebird.dev/"
+  xdg-open 'https://console.shorebird.dev/'
+fi
