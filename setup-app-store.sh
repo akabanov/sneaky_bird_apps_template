cd1efb7a51133b80783316cb0880911268b210f6
akabanov <alexander.kabanoff@gmail.com>
2025-01-18 14:28:34 +1300

    Retry failed app store setup steps with user prompts
    Added retry loops for critical Fastlane commands during App Store setup. Users are now prompted to manually skip steps if failures persist, improving resilience and offering more control during execution.

--- setup-app-store.sh
+++ setup-app-store.sh
@@ -5,13 +5,34 @@
 setup_app_store() {
   pushd 'ios' > /dev/null || return 1
 
-  bundle exec fastlane ios create
+  until bundle exec fastlane ios create; do
+    echo
+    read -n 1 -r -p "App Store project creation has failed. Skip? (y/N) " YN
+    echo
+    if [[ "$YN" =~ ^[yY] ]]; then
+      break
+    fi
+  done
 
   mv -f fastlane/Deliverfile fastlane/Deliverfile.stash
-  FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane deliver init --skip_screenshots
+  until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane deliver init --skip_screenshots; do
+    echo
+    read -n 1 -r -p "Fastlane 'deliver init' has failed. Skip? (y/N) " YN
+    echo
+    if [[ "$YN" =~ ^[yY] ]]; then
+      break
+    fi
+  done
   mv -f fastlane/Deliverfile.stash fastlane/Deliverfile
 
-  bundle exec fastlane ios deploy_meta
+  until bundle exec fastlane ios deploy_meta; do
+    echo
+    read -n 1 -r -p "Uploading initial app metadata has failed. Skip? (y/N) " YN
+    echo
+    if [[ "$YN" =~ ^[yY] ]]; then
+      break
+    fi
+  done
 
   popd > /dev/null || return 1
 }
