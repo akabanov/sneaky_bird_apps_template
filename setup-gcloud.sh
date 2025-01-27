4ee5d5dce512c9988da18bf7ed31a6c64b9c6066
akabanov <alexander.kabanoff@gmail.com>
2025-01-27 14:12:41 +1300

    setup-gcloud.sh: check if project already exists; fix bucket existence check

--- setup-gcloud.sh
+++ setup-gcloud.sh
@@ -2,8 +2,7 @@
 
 . .env
 
-echo
-echo "Make sure we're logged in"
+echo "Choosing an active account"
 GOOGLE_ACCOUNT=$(gcloud config get-value account 2>/dev/null)
 if [[ "$GOOGLE_ACCOUNT" == "(unset)" ]] || [[ -z "$GOOGLE_ACCOUNT" ]]; then
   echo "Not logged in. Starting authentication..."
@@ -13,25 +12,25 @@ else
   read -n 1 -r -p "Continue with this account? (Y/n) " YN && [[ "$YN" =~ ^[nN] ]] && gcloud auth login
   echo
 fi
-echo "Done"
 
-echo
-echo "Create Google Cloud project"
+
 gcloud config unset project
-gcloud projects create "${APP_ID_SLUG}" --name="${APP_NAME_DISPLAY}"
+echo "Creating Google Cloud project '${APP_NAME_DISPLAY}'; project ID: ${APP_ID_SLUG}"
+if ! gcloud projects list --format="value(project_id)" | grep -q "${APP_ID_SLUG}"; then
+  gcloud projects create "${APP_ID_SLUG}" --name="${APP_NAME_DISPLAY}"
+else
+  echo "Project ${APP_ID_SLUG} already exists"
+fi
 gcloud config set project "${APP_ID_SLUG}"
-echo "Project name: ${APP_NAME_SLUG}; project ID: ${APP_ID_SLUG}"
-echo "Done"
 
-echo
-echo "Enable required APIs"
+
+echo "Enabling required APIs"
 gcloud services enable testing.googleapis.com
 gcloud services enable toolresults.googleapis.com
 gcloud services enable billingbudgets.googleapis.com
-echo "Done"
 
-echo
-echo "Set up project billing account: https://console.cloud.google.com/billing"
+
+echo "Setting up project billing account: https://console.cloud.google.com/billing"
 read -n 1 -r -p "Open billing page? (y/N) " YN && [[ "$YN" =~ ^[yY] ]] && xdg-open 'https://console.cloud.google.com/billing' > /dev/null &
 echo
 echo "Current accounts:"
@@ -43,17 +42,17 @@ if [ -z "$BILLING_ACCOUNT_ID" ]; then
   exit 1
 fi
 gcloud billing projects link "${APP_ID_SLUG}" --billing-account="${BILLING_ACCOUNT_ID}"
-echo "Done"
 
-echo
-echo "Configuring Google storage for Firebase Test Lab"
+
+echo "Creating a bucket for Firebase Test Lab"
 TEST_LAB_BUCKET_NAME="gs://${APP_ID_SLUG}-test"
-if ! gcloud storage buckets list --filter="name:${TEST_LAB_BUCKET_NAME}" --format="value(name)" | grep -q "${TEST_LAB_BUCKET_NAME}"; then
+if ! gcloud storage buckets list --filter="name=${TEST_LAB_BUCKET_NAME}" --format="value(name)" | grep -q "${TEST_LAB_BUCKET_NAME}"; then
   gcloud storage buckets create "${TEST_LAB_BUCKET_NAME}" --location US-WEST1 --public-access-prevention --uniform-bucket-level-access
+else
+  echo "Bucket ${TEST_LAB_BUCKET_NAME} already exists"
 fi
-echo "Done"
 
-echo
+
 echo "Adding permissions for Firebase Test Lab"
 GOOGLE_ACCOUNT=$(gcloud config get-value account 2>/dev/null)
 gcloud projects add-iam-policy-binding "${APP_ID_SLUG}" \
@@ -62,4 +61,3 @@ gcloud projects add-iam-policy-binding "${APP_ID_SLUG}" \
 gcloud projects add-iam-policy-binding "${APP_ID_SLUG}" \
     --member="user:$GOOGLE_ACCOUNT" \
     --role="roles/firebase.analyticsViewer"
-echo "Done"
