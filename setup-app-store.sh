4206a6ae98e90ee8a05dd895abe8dab78a9f87f9
akabanov <alexander.kabanoff@gmail.com>
2025-02-04 20:33:36 +1300

    add OneSignal integration: iOS setup

--- setup-app-store.sh
+++ setup-app-store.sh
@@ -6,32 +6,35 @@ setup_app_store() {
   pushd 'ios' > /dev/null || return 1
 
   until bundle exec fastlane ios create; do
-    echo
-    read -n 1 -r -p "App Store project creation has failed. Skip? (y/N) " YN
-    echo
-    if [[ "$YN" =~ ^[yY] ]]; then
-      break
-    fi
+    echo "Retrying in 3 seconds "
+    sleep 3
+  done
+
+  until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane produce enable_services --app_group --push-notification; do
+    echo "Retrying in 3 seconds "
+    sleep 3
+  done
+
+  until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane produce group -g "group.${BUNDLE_ID}.onesignal" -n "${APP_LABEL_DASHBOARDS} OneSignal"; do
+    echo "Retrying in 3 seconds "
+    sleep 3
+  done
+
+  until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane produce associate_group -a "$BUNDLE_ID" "group.${BUNDLE_ID}.onesignal"; do
+    echo "Retrying in 3 seconds "
+    sleep 3
   done
 
   mv -f fastlane/Deliverfile fastlane/Deliverfile.stash
   until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane deliver init --skip_screenshots; do
-    echo
-    read -n 1 -r -p "Fastlane 'deliver init' has failed. Skip? (y/N) " YN
-    echo
-    if [[ "$YN" =~ ^[yY] ]]; then
-      break
-    fi
+    echo "Retrying in 3 seconds "
+    sleep 3
   done
   mv -f fastlane/Deliverfile.stash fastlane/Deliverfile
 
   until bundle exec fastlane ios deploy_meta; do
-    echo
-    read -n 1 -r -p "Uploading initial app metadata has failed. Skip? (y/N) " YN
-    echo
-    if [[ "$YN" =~ ^[yY] ]]; then
-      break
-    fi
+    echo "Retrying in 3 seconds "
+    sleep 3
   done
 
   popd > /dev/null || return 1
