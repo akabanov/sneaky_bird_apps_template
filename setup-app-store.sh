4d15feb1c039fd678ab19263fc2a78c6856e9c56
akabanov <alexander.kabanoff@gmail.com>
2025-02-07 20:32:55 +1300

    fastlane ios: add empty metadata files
    fix (clean) metadata

--- setup-app-store.sh
+++ setup-app-store.sh
@@ -10,18 +10,6 @@ setup_app_store() {
     sleep 3
   done
 
-  mv -f fastlane/Deliverfile fastlane/Deliverfile.stash
-  until FASTLANE_PASSWORD=$(cat "$ITUNES_PASSWORD_PATH") bundle exec fastlane deliver init --skip_screenshots; do
-    echo "Retrying in 3 seconds "
-    sleep 3
-  done
-  mv -f fastlane/Deliverfile.stash fastlane/Deliverfile
-
-  until bundle exec fastlane ios deploy_meta; do
-    echo "Retrying in 3 seconds "
-    sleep 3
-  done
-
   popd > /dev/null || return 1
 }
 
