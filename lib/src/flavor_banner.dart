1dec92800874c5b49643f8f87ec72bc3b574d72b
akabanov <alexander.kabanoff@gmail.com>
2025-06-10 13:04:09 +0400

    flavors: minor refactoring

--- lib/src/flavor_banner.dart
+++ lib/src/flavor_banner.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FlavorBanner extends StatelessWidget {
+  static const flavor = appFlavor ?? 'bland';
+
   const FlavorBanner({super.key, required this.child});
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    if (appFlavor == null || appFlavor == 'prod') {
+    if (flavor == 'prod') {
       return child;
     }
+
     return Banner(
+      message: flavor,
       location: BannerLocation.bottomEnd,
-      message: appFlavor!,
       color: Colors.green.withValues(alpha: 0.6),
       textStyle: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 12.0,
-          letterSpacing: 1.0),
+        fontWeight: FontWeight.w700,
+        fontSize: 12.0,
+        letterSpacing: 1.0,
+      ),
       textDirection: TextDirection.ltr,
       child: child,
     );
