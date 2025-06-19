0e1bcb21198fe23ad9772a53997c07dc93718be3
akabanov <alexander.kabanoff@gmail.com>
2025-06-19 17:32:30 +0400

    main.dart: only init OneSignal if running in ios or android

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:sneaky_bird_apps_template/src/app.dart';
 import 'package:sneaky_bird_apps_template/src/env.dart';
+import 'dart:io';
 
 void main() async {
   tryInitOneSignal();
@@ -10,7 +11,7 @@ void main() async {
 
 void tryInitOneSignal() {
   const appId = Env.oneSignalAppId;
-  if (appId.isNotEmpty) {
+  if (appId.isNotEmpty && (Platform.isIOS || Platform.isAndroid)) {
     // OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
     OneSignal.initialize(appId);
   }
