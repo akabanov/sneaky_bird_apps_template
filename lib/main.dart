c2add8a6a934c545c674c5651c501313f6a12577
akabanov <alexander.kabanoff@gmail.com>
2025-06-26 18:24:37 +0200

    main.dart & main.dart.sentry update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:sneaky_bird_apps_template/src/app.dart';
@@ -5,6 +6,11 @@ import 'package:sneaky_bird_apps_template/src/env.dart';
 import 'dart:io';
 
 void main() async {
+  throw Exception("Can't run flavorless; use main_{flavor}.dart entry points");
+}
+
+Future<void> runMainApp(AsyncCallback appInit) async {
+  await appInit();
   tryInitOneSignal();
   runApp(App());
 }
